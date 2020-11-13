652e753f900d4c384385a2f6113ab8d4b117ea9d
Andre da Silva <andrezio@live.com>
2020-11-12 21:51:03 -0300

    update correction function
    lorentz polarization
    centralization peak

---
+++ README.md
@@ -0,0 +1,20 @@
+# XPA: x-ray profile analysis
+
+This lib is developed like a tool for x-ray profile analysis,
+for apply corrections and apply the methods for analysis of crystalit size and microdeformation.
+
+## How to install
+
+For instalation is recomended creat a virtualenv
+
+virtual env (optional)
+```python
+python3 -m venv env
+source env/bin/activate
+```
+Install XPA lib
+```python
+pip install xpa
+```
+# Usage
+
--- xpa/correction.py
+++ xpa/correction.py
@@ -1,29 +1,35 @@
 import numpy as np
-from math import factorial
 from math import sin, cos, pi, radians, tan, sqrt, log1p
 from scipy import stats
 
 
-def Ka2(x,y):
-    novoy=[]
-    lambida2=1.541220
-    lambida1=1.537400
+def get_interval(x, y, start, end):
+    init = x.index(start)
+    end = x.index(end)
+
+    return x[init:end], y[init:end]
+
+
+def remove_kalpha2(x, y):
+    novoy = []
+    lambida2 = 1.541220
+    lambida1 = 1.537400
     deltaL = lambida2 - lambida1
-    deltaL = deltaL/lambida1
-    diferenca=x[1]-x[0]
+    deltaL = deltaL / lambida1
+    diferenca = x[1] - x[0]
 
     for i in range(len(y)):
-        deltasoma = x[1]-x[0]
-        ase = np.tan(np.radians(x[i]/2))*2*deltaL/(diferenca)
-        n=1
+        deltasoma = x[1] - x[0]
+        ase = np.tan(np.radians(x[i] / 2)) * 2 * deltaL / (diferenca)
+        n = 1
 
-        while(ase>deltasoma):
+        while (ase > deltasoma):
             deltasoma = deltasoma + diferenca
-            n +=1
+            n += 1
         try:
-            yy=y[i]-0.5*y[i-n]
+            yy = y[i] - 0.5 * y[i - n]
 
-            if yy<0:yy=(yy+y[i])/8
+            if yy < 0: yy = (yy + y[i]) / 8
 
             novoy.append(yy)
         except:
@@ -32,99 +38,41 @@ def Ka2(x,y):
     return novoy
 
 
-def BG(x,y,m=5):
-##    y=normalizar(y)
-
-    minimo= np.mean( np.sort(y)[:10])
+def remove_background(x, y, m=5):
+    min_value = np.mean(np.sort(y)[:10])
     for i in range(len(y)):
-        y[i]=y[i]-minimo
-    slope, intercept, r_value, p_value, std_err = stats.linregress(np.append(x[:m],x[-m:]),np.append(y[:m],y[-m:]))
-    abline_values = [slope * i + intercept for i in x]
-    abline_values=np.asarray(abline_values)
-    return (y-abline_values)
+        y[i] = y[i] - min_value
+    slope, intercept, r_value, p_value, std_err = stats.linregress(np.append(x[:m], x[-m:]), np.append(y[:m], y[-m:]))
+    line_values = [slope * i + intercept for i in x]
+    line_values = np.asarray(line_values)
+    return y - line_values
 
 
 def normalize(vector):
     list_temp = []
     max_value = max(vector)
     for value in vector:
-        list_temp.append(value/max_value)
+        list_temp.append(value / max_value)
     return list_temp
 
 
-def savitzky_golay(y, window_size, order, deriv=0, rate=1):
-    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-    The Savitzky-Golay filter removes high frequency noise from data.
-    It has the advantage of preserving the original shape and
-    features of the signal better than other types of filtering
-    approaches, such as moving averages techniques.
-    Parameters
-    ----------
-    y : array_like, shape (N,)
-        the values of the time history of the signal.
-    window_size : int
-        the length of the window. Must be an odd integer number.
-    order : int
-        the order of the polynomial used in the filtering.
-        Must be less then `window_size` - 1.
-    deriv: int
-        the order of the derivative to compute (default = 0 means only smoothing)
-    Returns
-    -------
-    ys : ndarray, shape (N)
-        the smoothed signal (or it's n-th derivative).
-    Notes
-    -----
-    The Savitzky-Golay is a type of low-pass filter, particularly
-    suited for smoothing noisy data. The main idea behind this
-    approach is to make for each point a least-square fit with a
-    polynomial of high order over a odd-sized window centered at
-    the point.
-    Examples
-    --------
-    t = np.linspace(-4, 4, 500)
-    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
-    ysg = savitzky_golay(y, window_size=31, order=4)
-    import matplotlib.pyplot as plt
-    plt.plot(t, y, label='Noisy signal')
-    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
-    plt.plot(t, ysg, 'r', label='Filtered signal')
-    plt.legend()
-    plt.show()
-    References
-    ----------
-    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-       Data by Simplified Least Squares Procedures. Analytical
-       Chemistry, 1964, 36 (8), pp 1627-1639.
-    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
-       Cambridge University Press ISBN-13: 9780521880688
-    """
-    try:
-        window_size = np.abs(np.int(window_size))
-        order = np.abs(np.int(order))
-    except ValueError as msg:
-        raise ValueError("window_size and order have to be of type int")
-    if window_size % 2 != 1 or window_size < 1:
-        raise TypeError("window_size size must be a positive odd number")
-    if window_size < order + 2:
-        raise TypeError("window_size is too small for the polynomials order")
-    order_range = range(order+1)
-    half_window = (window_size - 1) // 2
-    # precompute coefficients
-    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
-    # pad the signal at the extremes with
-    # values taken from the signal itself
-    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-    y = np.concatenate((firstvals, y, lastvals))
-    return np.convolve( m[::-1], y, mode='valid')
-
-
-def lorentz_and_polarization(x,y):
-
+def lorentz_and_polarization(x, y):
     for i in range(len(y)):
-        y[i]/=(1+pow(cos(radians( x[i])),2))/(  cos( radians( x[i]))*pow(sin( radians( x[i])),2))
+        y[i] /= (1 + pow(cos(radians(x[i])), 2)) / (cos(radians(x[i])) * pow(sin(radians(x[i])), 2))
 
     return y
+
+
+def centralizer(y1):
+    middle = list(y1).index(max(y1))
+    left = list(y1).index(max(y1))
+    right = abs(list(y1).index(max(y1)) - len(y1))
+    dif = abs(left - right)
+    if left < right:
+        new_vector = dif * [y1[0]] + list(y1[:middle - 1]) + list([y1[middle]]) + list(y1[middle + 1:])
+    else:
+        new_vector = list(y1[:middle - 1]) + list([y1[middle]]) + list(y1[middle + 1:]) + dif * [y1[-1]]
+
+    new_vector = 50 * [y1[0]] + new_vector + 50 * [y1[-1]]
+
+    return new_vector
